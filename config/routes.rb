e7339bfb7252b12efd520c9459f314ce77cab260
Sunil Rao <sunil2rao@gmail.com>
2013-01-07 13:11:01 -0600

    use can create stores

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Ddk::Application.routes.draw do
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   authenticated :user do
     root :to => 'home#index'
   end
   root :to => "home#index"
   devise_for :users
-  resources :users
+  resources :users do
+    resources :stores
+  end
+  
 end
