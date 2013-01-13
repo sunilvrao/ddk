09e2bb1c6d154abe37609a62ee034569276ff265
Sunil Rao <sunil2rao@gmail.com>
2013-01-13 00:05:54 -0600

    asdad

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,11 @@ class ApplicationController < ActionController::Base
     redirect_to root_path, :alert => exception.message
   end
   
+  #after_sign_in_path_for is called by devise
+  def after_sign_in_path_for(resource_or_scope)
+    dashboards_path
+  end
+  
   # Override build_footer method in ActiveAdmin::Views::Pages
   require 'active_admin_views_pages_base.rb'
 end
